183ad9e21c1b5345576ad1477adca2bfb1c00470
Yule <doctoryule@aol.com>
2011-01-14 11:56:46 +0000

    initial content structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 DryuleCom::Application.routes.draw do
+
+  
+
   root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match 'about-me' => 'about_me#index'
+  match 'sport' => 'sport#index'
+  match 'poker' => 'poker#index'
+  match 'work' => 'work#index'
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -47,10 +55,6 @@ DryuleCom::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
