03809472c300198bb154351c1b34f2f03e463517
paul <dr_yule@hotmail.com>
2011-03-05 13:13:29 +0000

    git fuckups

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require File.expand_path('../config/application', __FILE__)
+require "#{RAILS_ROOT}/vendor/plugins/easypg/lib/override_rake_task.rb"
 
 require 'rake'
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,22 +9,4 @@ $(document).ready(function(){
     $(this).removeClass('rollover')
   });
   
-  
-  var keyList = '';
-  var correct = '38#38#40#40#37#39#37#39#66#65#'
-  $(document).keydown(function(e){
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == 38 && keyList != '38#') { 
-      keyList = '';
-    }
-    keyList += keyCode + '#';
-    if (keyList == correct){
-      $('#header').append("<div id='ninja-div' style='position:absolute; width: 300px; top:0; left:0;z-index:1000;background-color: #ffffff;'></div>");
-      $(document).keydown(function(e){
-        $('#ninja-div').append(char= String.fromCharCode(e.which));
-        e.preventDefault();
-      });
-    }
-  });
-  
 });
